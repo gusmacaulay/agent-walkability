7e570cf301ceac5da3d708842288373f38266187
Gus MacAulay <angus.macaulay@unimelb.edu.au>
2012-11-16 17:15:06 +1100

    Added test datasets for multi destination simulation.  Moddified Steps to load road network.

--- src/test/java/org/mccaughey/PathGeneratorSteps.java
+++ src/test/java/org/mccaughey/PathGeneratorSteps.java
@@ -2,13 +2,19 @@ package org.mccaughey;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 import junit.framework.Assert;
 
+import org.geotools.data.DataUtilities;
 import org.geotools.data.FileDataStore;
 import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.feature.FeatureCollections;
+import org.geotools.feature.FeatureIterator;
+import org.geotools.geojson.feature.FeatureJSON;
 import org.geotools.graph.path.Path;
 import org.geotools.graph.structure.Edge;
 import org.jbehave.core.annotations.Given;
@@ -16,6 +22,7 @@ import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.jbehave.core.embedder.Embedder;
 import org.mccaughey.pathGenerator.PathGenerator;
+import org.opengis.feature.simple.SimpleFeature;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
@@ -39,8 +46,12 @@ public class PathGeneratorSteps extends Embedder {
 
   @Given("a footpath network")
   public void aFootpathNetwork() throws IOException {
-    File networkShapeFile = new File("/home/amacaulay/SRC/agent-walkability/"
-        + "non-free-data/Footpaths_simple.shp");
+//    File networkShapeFile = new File("/home/amacaulay/SRC/agent-walkability/"
+//        + "non-free-data/Footpaths_simple.shp");
+    File networkShapeFile = new File(getClass().getResource("/melton_roads_sample.shp").getFile());
+//      URL footpathURL = getClass().getResource("/melton_road_sample.geojson");
+//      System.out.println("URL:" + footpathURL);
+//      networkSource = DataUtilities.source(readFeatures(footpathURL));
     FileDataStore networkDataStore = FileDataStoreFinder
         .getDataStore(networkShapeFile);
     // ShapefileDataStore store = new
@@ -53,23 +64,23 @@ public class PathGeneratorSteps extends Embedder {
 
   @Given("a startpoint")
   public void aStartPoint() {
-    double easting = 286080;
-    double northing = 5821102;
+    double easting = 285752;
+    double northing = 5824386;
     start = geometryFactory.createPoint(new Coordinate(easting, northing));
   }
 
   @Given("an endpoint")
   public void anEndPoint() {
-    double easting = 286074.21;
-    double northing = 5821166.81;
+    double easting = 285545;
+    double northing = 5825011;
     end = geometryFactory.createPoint(new Coordinate(easting, northing));
 
   }
   
   @Given("a set of endpoints")
   public void aSetOfEndPoints() {
-    double easting = 286074.21;
-    double northing = 5821166.81;
+    double easting = 285545;
+    double northing = 5825011;
     end = geometryFactory.createPoint(new Coordinate(easting, northing));
 
   }
@@ -93,6 +104,24 @@ public class PathGeneratorSteps extends Embedder {
       System.out.println(line.toText());
     }
     // Assert.assertTrue(path != null);
+  }
+  
+  private static SimpleFeatureCollection readFeatures(URL url)
+      throws IOException {
+    FeatureJSON io = new FeatureJSON();
+    // io.setEncodeFeatureCollectionCRS(true);
+
+    // io.readCRS(url.openConnection().getInputStream()));
+    FeatureIterator<SimpleFeature> features = io.streamFeatureCollection(url
+        .openConnection().getInputStream());
+    SimpleFeatureCollection collection = FeatureCollections.newCollection();
+
+    while (features.hasNext()) {
+      SimpleFeature feature = (SimpleFeature) features.next();
+      collection.add(feature);
+    }
+
+    return collection;
   }
 
 }
